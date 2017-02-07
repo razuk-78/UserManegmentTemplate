2202e038220de67b884d935195625d501d492c61
raeli1 <raeli1@LPI-CND40804H3>
2017-02-07 13:08:43 +0100

    SecondDataBaesUpdate

---
+++ UserMangementTemplate/UserMangementTemplate/Security/SearchBasedUser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserMangementTemplate.Models;
+namespace UserMangementTemplate.Security
+{
+ 
+    public  class SearchBasedUser
+    {
+       
+  public  class UserDetails
+    {
+            public UserDetails()
+            {
+                Department = new department();
+                Teams = new List<Team>();
+                Auth = new List<Models.Auth>();
+                LogInRegistry = new List<Models.LogInRegistry>();
+            }
+        public Org Org { get; set; }
+        public User User { get; set; }
+        public department Department { get; set; }
+        public List<Team> Teams { get; set; }
+        public List<Auth>Auth { get; set; }
+        public List<LogInRegistry> LogInRegistry { get; set; }
+    }
+        public UserDetails searchUser(UMDataBaseEntitiesConnection db, int userid)
+        {
+
+            return new UserDetails();
+        }
+     
+        public List<UserDetails> searchTeam(UMDataBaseEntitiesConnection db, int TeamIid)
+        {
+
+            return new List<UserDetails>() ;
+        }
+
+        public List<UserDetails> searchOrg(UMDataBaseEntitiesConnection db, int OrgId)
+        {
+
+            return new List<UserDetails>();
+        }
+
+        public List<UserDetails> searchDep(UMDataBaseEntitiesConnection db, int DepId)
+        {
+
+            return new List<UserDetails>();
+        }
+
+        public List<UserDetails> searchAuth(UMDataBaseEntitiesConnection db, string Auth)
+        {
+
+            return new List<UserDetails>();
+        }
+
+        //One day
+        public List<UserDetails> searchAllUserBasedDate(UMDataBaseEntitiesConnection db, DateTime before)
+        {
+            
+            return new List<UserDetails>();
+        }
+
+        //between Two Date Based OrgId
+        public List<UserDetails> searchAllUserInOrgRangDate(UMDataBaseEntitiesConnection db,DateTime before, DateTime after ,int orgid)
+        {
+      
+            return new List<UserDetails>();
+        }
+
+        //between Two Date Based UserId
+        public List<UserDetails> searchRangDateBasedUserId(UMDataBaseEntitiesConnection db, DateTime before, DateTime after, int userId)
+        {
+          
+            return new List<UserDetails>();
+        }
+
+
+        public UserDetails SearchDetailsBasedUserIdTeamId(UMDataBaseEntitiesConnection db,int userId,int teamId)
+        {
+
+            return new UserDetails();
+        }
+
+        public UserDetails SearchDetailsBasedUserIdDepId(UMDataBaseEntitiesConnection db, int userId, int DepId)
+        {
+
+            return new UserDetails();
+        }
+
+
+
+
+    }
+}
---
+++ UserMangementTemplate/UserMangementTemplate/Security/SearchDepTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserMangementTemplate.Models;
+namespace UserMangementTemplate.Security
+{
+    public class SearchDepTree
+    {
+       public class Deps
+        {
+            public Deps()
+            {
+                Child = new List<int>();
+            }
+            public int Parent { get; set; }
+            public List<int> Child { get; set; }
+            public department Dep { get; set; }
+        }
+        public SearchDepTree()
+        {
+           
+        }
+
+        public List<Deps> DepsTree(UMDataBaseEntitiesConnection db,int depId)
+       
+        {
+            List<Deps> deps = new List<Deps>();
+
+           for(db.DepPointer.Where(x => x.ParentId == depId){
+
+            }
+            foreach (department dep in db.department)
+            {
+                
+            }
+            int p=0;int ch=0;int d = 0;
+        for(int i = 0; i < db.DepPointer.ToList().Count; i++)
+            {
+
+               
+            }
+           
+            return deps;
+        }
+       
+    }
+}
