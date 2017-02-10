1a3a002e984f5f198a8cb80fbd2c054fd0a70e05
raeli1 <raeli1@LPI-CND40804H3>
2017-02-10 16:21:25 +0100

    2017-02-10

---
+++ test/test/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace test.Controllers
+{
+    public class testController : ApiController
+    {
+        public IHttpActionResult Get(string ms)
+        {
+            return Ok(ms);
+        }
+    }
+}
