c1e03a3a4a38a18ef3340e116d0a76529e07883d
mumoh2 <mumoh2@LPI-CND446BXYD>
2017-02-17 11:15:09 +0100

    Login and home

---
+++ UserMangementTemplate/UserMangementTemplate/App/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+
+    
+    $locationProvider.hashPrefix('');
+    // default route
+
+
+    $routeProvider.when('/login', { //Routing for show list of employee
+        templateUrl: 'App/Views/Home/login.html',
+        controller: ''
+    })
+    $routeProvider.when('/home', { //Routing for show list of employee
+        templateUrl: 'App/Views/Home/Home.html',
+        controller: ''
+    })
+}]);
